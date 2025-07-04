49fd9547b2cbb8251d9c2e29dc5ad41b2d995848
Parikshit-web <23F3003781@ds.study.iitm.ac.in>
2025-07-04 11:54:22 -0700

    refined the models.py,config.py and app.py and created db.sqlite

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from config import Config
 from extension import db, migrate
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
+
 db.init_app(app)
 migrate.init_app(app, db)
   
@@ -14,6 +14,5 @@ from routes import *
 if __name__ == '__main__':  
     with app.app_context():
         db.create_all()
-        print("✅ Tables created, DB should exist at:", app.config['SQLALCHEMY_DATABASE_URI'])
 
     app.run(debug=True)  
